a441be1ec96c2d924dd8fcae4662cb791bc361ff
mayank surti <mayank@git>
2020-02-28 12:49:27 +0530

    Added Add RPC Screen, made changes in Strapi utilities, added severity property for alert

--- webapp/src/components/Alert/Alert.js
+++ webapp/src/components/Alert/Alert.js
@@ -2,5 +2,12 @@ import MuiAlert from "@material-ui/lab/Alert";
 import React from "react";
 
 export default function Alert(props) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
+  return (
+    <MuiAlert
+      elevation={6}
+      severity={props.severity}
+      variant="filled"
+      {...props}
+    />
+  );
 }
