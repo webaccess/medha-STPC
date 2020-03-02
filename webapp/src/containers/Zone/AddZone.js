dcedf2763ee58b8060182374975f552d0d077827
yogesh.dalvi@webaccessglobal.com <yogesh.dalvi@webaccessglobal.com>
2020-03-02 12:01:16 +0530

    Changes to revert the AddZone.js file.

--- webapp/src/containers/Zone/AddZone.js
+++ webapp/src/containers/Zone/AddZone.js
@@ -1,119 +1,258 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { InputText, Button } from "../../components";
-import Select from "@material-ui/core/Select";
-import Layout from "../../hoc/Layout/Layout.js";
-import { withStyles } from "@material-ui/core/styles";
-import * as strapiConstants from "../../constants/StrapiApiConstants";
-import { CustomRouterLink } from "../../components";
+import { get } from "lodash";
+import useStyles from "./AddZoneStyles";
+import * as strapiApiConstants from "../../constants/StrapiApiConstants";
+import AddZoneForm from "./AddZoneForm";
+import * as databaseUtilities from "../../Utilities/StrapiUtilities";
+import * as formUtilities from "../../Utilities/FormUtilities";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { CustomRouterLink, Alert } from "../../components";
+import * as genericConstants from "../../constants/GenericConstants";
 import * as routeConstants from "../../constants/RouteConstants";
 
-const useStyles = theme => ({
-  button: {
-    marginTop: "25px",
-    marginLeft: "75px"
-  },
-  align: {
-    marginLeft: "250px"
-  },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2)
-  }
-});
+import {
+  Card,
+  CardHeader,
+  CardActionArea,
+  CardContent,
+  CardActions,
+  TextField,
+  Grid,
+  Button,
+  Divider,
+  Typography
+} from "@material-ui/core";
 
-class AddZone extends Component {
-  getstates = [];
-  constructor() {
-    super();
-    this.state = {
-      addzone: "",
-      getstates: [],
-      stateid: 0
-    };
-  }
-  handleChange(event) {
-    this.setState({ addzone: event.target.value });
-  }
-  handleStateChange(event) {
-    this.setState({ stateid: event.target.value });
-  }
-  handleSubmit(event) {
-    event.preventDefault();
-    var name = this.state.addzone;
-    var ID = this.state.stateid;
+const AddZone = props => {
+  const zone = "zone";
+  const state = "state";
+  const content = "content";
+  const classes = useStyles();
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isFailed, setIsFailed] = useState(false);
+
+  const [formState, setFormState] = useState({
+    isValid: false,
+    values: {},
+    touched: {},
+    errors: {},
+    isSuccess: false
+  });
+
+  const [states, setStates] = useState([]);
+
+  useEffect(() => {
     axios
-      .post(strapiConstants.STRAPI_DB_URL + strapiConstants.STRAPI_ZONES, {
-        name: name,
-        state: {
-          id: ID
-        }
-      })
+      .get(strapiApiConstants.STRAPI_DB_URL + strapiApiConstants.STRAPI_STATES)
       .then(res => {
-        window.location.reload();
+        setStates(res.data);
+      });
+  }, []);
+
+  const handleChange = e => {
+    e.persist();
+    setFormState(formState => ({
+      ...formState,
+      values: {
+        ...formState.values,
+        [e.target.name]: e.target.value
+      },
+      touched: {
+        ...formState.touched,
+        [e.target.name]: true
+      }
+    }));
+    if (formState.errors.hasOwnProperty(e.target.name)) {
+      delete formState.errors[e.target.name];
+    }
+  };
+
+  const handleChangeAutoComplete = (eventName, event, value) => {
+    if (value !== null) {
+      setFormState(formState => ({
+        ...formState,
+        values: {
+          ...formState.values,
+          [eventName]: value.id
+        },
+        touched: {
+          ...formState.touched,
+          [eventName]: true
+        }
+      }));
+      if (formState.errors.hasOwnProperty(eventName)) {
+        delete formState.errors[eventName];
+      }
+    } else {
+      delete formState.values[eventName];
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    let isValid = false;
+    let checkAllFieldsValid = formUtilities.checkAllKeysPresent(
+      formState.values,
+      AddZoneForm
+    );
+    if (checkAllFieldsValid) {
+      formState.errors = formUtilities.setErrors(formState.values, AddZoneForm);
+      if (formUtilities.checkEmpty(formState.errors)) {
+        isValid = true;
+      }
+    } else {
+      formState.values = formUtilities.getListOfKeysNotPresent(
+        formState.values,
+        AddZoneForm
+      );
+      formState.errors = formUtilities.setErrors(formState.values, AddZoneForm);
+      console.log("not all keys present ", formState);
+    }
+    console.log(isValid, formState);
+    if (isValid) {
+      console.log("formValid");
+      postZoneData();
+
+      /** Call axios from here */
+      setFormState(formState => ({
+        ...formState,
+        isValid: true
+      }));
+    } else {
+      console.log("formInValid");
+      setFormState(formState => ({
+        ...formState,
+        isValid: false
+      }));
+    }
+  };
+
+  const postZoneData = async () => {
+    let postData = databaseUtilities.addZone(
+      formState.values[zone],
+      formState.values[state]
+        ? databaseUtilities.setState(formState.values[state])
+        : null
+    );
+    axios({
+      method: "post",
+      async: false,
+      url: strapiApiConstants.STRAPI_DB_URL + strapiApiConstants.STRAPI_ZONES,
+      data: postData
+    })
+      .then(res => {
+        console.log(res);
+        setIsFailed(false);
+        setIsSuccess(true);
       })
       .catch(error => {
-        console.log("Error > ", error.response);
+        console.log(error);
+        setIsSuccess(false);
+        setIsFailed(true);
       });
-  }
-  componentDidMount() {
-    axios
-      .get(strapiConstants.STRAPI_DB_URL + strapiConstants.STRAPI_STATES)
-      .then(res => {
-        this.setState({ getstates: res.data });
-        this.getstates = res.data;
-      });
-  }
-  render() {
-    const { classes } = this.props;
-    return (
-      <div>
-        <Layout>
-          <h1>Zone</h1>
-          <form onSubmit={this.handleSubmit.bind(this)}>
-            <span>Zone Name </span>
-            <InputText
-              value={this.state.addzone}
-              name="addzone"
-              onChange={this.handleChange.bind(this)}
-              placeholder="Add Zone"
-            />
-            <br></br>
-            <br></br>
-            <span>State Name </span>
-            <Select
-              placeholder="Add Zone"
-              onChange={this.handleStateChange.bind(this)}
-            >
-              {this.state.getstates.map(states => (
-                <option value={states.id} key={states.id}>
-                  {" "}
-                  {states.name}{" "}
-                </option>
-              ))}
-            </Select>
-            <br></br>
-            <br></br>
-            <Button buttonType="submit" value="submit">
-              ADD
-            </Button>{" "}
-            <Button
-              variant="contained"
-              color="primary"
-              disableElevation
-              component={CustomRouterLink}
-              to={routeConstants.VIEW_ZONES}
-            >
-              Cancel
-            </Button>
-          </form>
-        </Layout>
-      </div>
-    );
-  }
-}
 
-export default withStyles(useStyles)(AddZone);
+    /** Set state to reload form */
+    setFormState(formState => ({
+      ...formState,
+      isValid: true
+    }));
+  };
+
+  const hasError = field => (formState.errors[field] ? true : false);
+
+  return (
+    <Grid>
+      <Grid item xs={12} className={classes.title}>
+        <Typography variant="h4" gutterBottom>
+          {get(AddZoneForm[content], "title")}
+        </Typography>
+      </Grid>
+
+      <Grid item xs={12} className={classes.formgrid}>
+        {isSuccess ? (
+          <Alert severity="success" className={classes.message}>
+            {genericConstants.ALERT_SUCCESS_BUTTON_MESSAGE}
+          </Alert>
+        ) : null}
+        {isFailed ? (
+          <Alert severity="error">
+            {genericConstants.ALERT_ERROR_BUTTON_MESSAGE}
+          </Alert>
+        ) : null}
+
+        <Card className={classes.root} variant="outlined">
+          <form autoComplete="off" noValidate onSubmit={handleSubmit}>
+            <CardHeader />
+            <CardActionArea>
+              <CardContent>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <TextField
+                      label={get(AddZoneForm[zone], "label")}
+                      name={zone}
+                      value={formState.values[zone] || ""}
+                      error={hasError(zone)}
+                      variant="outlined"
+                      required
+                      fullWidth
+                      onChange={handleChange}
+                      helperText={
+                        hasError(zone)
+                          ? formState.errors[zone].map(error => {
+                              return error + " ";
+                            })
+                          : null
+                      }
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Autocomplete
+                      id="combo-box-demo"
+                      className={classes.root}
+                      options={states}
+                      getOptionLabel={option => option.name}
+                      onChange={(event, value) => {
+                        handleChangeAutoComplete(state, event, value);
+                      }}
+                      renderInput={params => (
+                        <TextField
+                          {...params}
+                          label={get(AddZoneForm[state], "label")}
+                          variant="outlined"
+                          error={hasError(state)}
+                          helperText={
+                            hasError(state)
+                              ? formState.errors[state].map(error => {
+                                  return error + " ";
+                                })
+                              : null
+                          }
+                        />
+                      )}
+                    />
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </CardActionArea>
+            <Divider />
+            <CardActions>
+              <Button variant="contained" color="primary" type="submit">
+                {get(AddZoneForm[content], "button")}
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                component={CustomRouterLink}
+                to={routeConstants.VIEW_ZONES}
+              >
+                {get(AddZoneForm[content], "cancel")}
+              </Button>
+            </CardActions>
+          </form>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
+export default AddZone;
