c32bd7183a29789781c08392695ffa0ff0edfe08
shreesh.nirgudkar@gmail.com <shreesh.nirgudkar@gmail.com>
2020-03-13 16:02:11 +0530

    Updated validate policies for user, rpc, zone and college

--- backend/strapi/medha/api/state/config/policies/validate.js
+++ backend/strapi/medha/api/state/config/policies/validate.js
@@ -8,7 +8,7 @@ module.exports = async (ctx, next) => {
   // Add your own logic here.
 
   const { name } = ctx.request.body;
-  if (!name) ctx.response.badRequest("Name field is missing");
+  if (!name) return ctx.response.badRequest("Name field is missing");
 
   await next();
 };
