852e9129bb109eee0227d7c934f1415c565390af
Milind Jathar <milind.jathar@webaccessglobal.com>
2020-03-19 21:21:45 +0530

    created a Dashboard and css formatting done

--- webapp/src/containers/Dashboard/Styles.js
+++ webapp/src/containers/Dashboard/Styles.js
@@ -11,8 +11,19 @@ const useStyles = makeStyles(theme => ({
     maxWidth: "100%",
     width: 560
   },
-  root: {
-    padding: theme.spacing(4)
+  title: {
+    fontSize: "18px",
+    fontWeight: 700
+  },
+
+  h1: {
+    textAlign: "center !important",
+    fontSize: "26px !important",
+    fontWeight: 700
+  },
+  root: {},
+  table: {
+    minWidth: 300
   }
 }));
 
