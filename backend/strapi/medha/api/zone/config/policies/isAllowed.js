70842ccb155210f206567a5ab714dd0a27b21db8
shreesh.nirgudkar@gmail.com <shreesh.nirgudkar@gmail.com>
2020-03-20 17:04:52 +0530

    Added deleting policy for district state and zone

--- backend/strapi/medha/api/zone/config/policies/isAllowed.js
+++ backend/strapi/medha/api/zone/config/policies/isAllowed.js
@@ -3,15 +3,11 @@
 /**
  * `isAllowed` policy.
  */
-const bookshelf = require("../../../../config/config.js");
 module.exports = async (ctx, next) => {
   const { role, zone } = ctx.state.user;
   const { id } = ctx.params;
   if (role.name === "Zonal Admin") {
-    const data = await bookshelf
-      .model("zone")
-      .where({ id: id })
-      .fetch();
+    const data = await strapi.query("zone").findOne({ id });
 
     if (!data) {
       return ctx.response.notFound("Requested Zone does not exist");
