55d7b62e8e36c9412db37b48f568787bf09d2f1e
vibhavari <vibhavari.sathe@webaccessglobal.com>
2020-04-12 19:29:21 +0530

    Changes for setting up UI of AddEditEvent pafe and EventListings page

--- webapp/src/components/CustomDateTimePicker/CustomDateTimePickerStyles.js
+++ webapp/src/components/CustomDateTimePicker/CustomDateTimePickerStyles.js
@@ -4,35 +4,45 @@ const customTheme = createMuiTheme({
   overrides: {
     MuiPickersToolbar: {
       toolbar: {
-        backgroundColor: "black"
-      }
+        backgroundColor: "black",
+      },
+    },
+    MuiInputBase: {
+      root: {
+        fontSize: "14px",
+      },
+    },
+    MuiFormLabel: {
+      root: {
+        color: "#000",
+      },
     },
     MuiButton: {
       textPrimary: {
-        color: "#000"
-      }
+        color: "#000",
+      },
     },
     MuiPickersClock: {
       pin: {
-        backgroundColor: "#000"
-      }
+        backgroundColor: "#000",
+      },
     },
     MuiPickersClockPointer: {
       pointer: {
-        backgroundColor: "#000"
+        backgroundColor: "#000",
       },
       thumb: {
         backgroundColor: "#000",
-        border: "14px solid #000"
+        border: "14px solid #000",
       },
       noPoint: {
-        backgroundColor: "#000"
-      }
+        backgroundColor: "#000",
+      },
     },
     MuiPickersClockNumber: {
       clockNumberSelected: {
-        backgroundColor: "#000"
-      }
+        backgroundColor: "#000",
+      },
     },
 
     MuiPickerDTTabs: { tabs: { backgroundColor: "black" } },
@@ -42,17 +52,17 @@ const customTheme = createMuiTheme({
         color: "#fff",
         "&:hover": {
           backgroundColor: "#8E8E8E",
-          color: "#FFF"
-        }
+          color: "#FFF",
+        },
       },
       dayDisabled: {
-        color: "light-gray"
+        color: "light-gray",
       },
       current: {
-        color: ""
-      }
-    }
-  }
+        color: "",
+      },
+    },
+  },
 });
 
 export default customTheme;
