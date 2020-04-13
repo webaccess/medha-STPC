9294873d21af791f4499a96ad9d7e26df672b3d8
yogesh.dalvi@webaccessglobal.com <yogesh.dalvi@webaccessglobal.com>
2020-04-13 16:25:46 +0530

    Added a new route for College Admin. Update App.js

---
+++ webapp/src/authroute/CollegeAdminRoute.js
@@ -0,0 +1,49 @@
+import React from "../../node_modules/react";
+import { Route, Redirect } from "../../node_modules/react-router-dom";
+import PropTypes from "../../node_modules/prop-types";
+import auth from "../components/Auth/Auth";
+import * as routeConstants from "../constants/RouteConstants";
+
+const CollegeAdminRoute = props => {
+  const { layout: Layout, component: Component, ...rest } = props;
+  if (auth.getToken() !== null) {
+    if (auth.getUserInfo().role.name === "College Admin") {
+      return (
+        <Route
+          {...rest}
+          render={matchProps => (
+            <Layout>
+              <Component {...matchProps} />
+            </Layout>
+          )}
+        />
+      );
+    } else {
+      return (
+        <Redirect
+          to={{
+            pathname: routeConstants.NOT_FOUND_URL,
+            state: { from: props.location }
+          }}
+        />
+      );
+    }
+  } else {
+    return (
+      <Redirect
+        to={{
+          pathname: routeConstants.SIGN_IN_URL,
+          state: { from: props.location }
+        }}
+      />
+    );
+  }
+};
+
+CollegeAdminRoute.propTypes = {
+  component: PropTypes.any.isRequired,
+  layout: PropTypes.any.isRequired,
+  path: PropTypes.string
+};
+
+export default CollegeAdminRoute;
