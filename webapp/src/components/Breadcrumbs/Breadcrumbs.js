013761c23084e11b6617c2240a9d70164c0a8f33
shreesh.nirgudkar@gmail.com <shreesh.nirgudkar@gmail.com>
2020-04-29 17:42:33 +0530

    Added styles for breadcrumbs

--- webapp/src/components/Breadcrumbs/Breadcrumbs.js
+++ webapp/src/components/Breadcrumbs/Breadcrumbs.js
@@ -1,12 +1,36 @@
 import React from "react";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
-import Link from "@material-ui/core/Link";
 import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
 import { Typography } from "@material-ui/core";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import Chip from "@material-ui/core/Chip";
+import { emphasize, withStyles, makeStyles } from "@material-ui/core/styles";
+
+const StyledBreadcrumb = withStyles(theme => ({
+  root: {
+    backgroundColor: "#666666",
+    height: theme.spacing(3),
+    color: "white",
+    fontWeight: theme.typography.fontWeightRegular,
+    "&:hover, &:focus": {
+      backgroundColor: "#f6c80a"
+    },
+    "&:active": {
+      boxShadow: theme.shadows[1],
+      backgroundColor: emphasize("#666666", 0.12)
+    }
+  }
+}))(Chip);
+
+const styles = makeStyles(() => ({
+  root: {
+    paddingTop: "8px"
+  }
+}));
 
 const ActiveLastBreadcrumbs = props => {
+  const classes = styles();
   let history = useHistory();
   const handleClick = url => {
     history.push({
@@ -19,10 +43,14 @@ const ActiveLastBreadcrumbs = props => {
     <Breadcrumbs
       aria-label="breadcrumb"
       separator={<NavigateNextIcon fontSize="small" />}
+      className={classes.root}
     >
       {props.list.map((item, index) => {
         return index !== size ? (
-          <Link onClick={() => handleClick(item.href)}>{item.title}</Link>
+          <StyledBreadcrumb
+            onClick={() => handleClick(item.href)}
+            label={item.title}
+          />
         ) : (
           <Typography color="textPrimary">{item.title}</Typography>
         );
