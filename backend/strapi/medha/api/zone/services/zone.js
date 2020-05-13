02d00f270c5e44f589f0102cfca6cd3b488f67e5
shreesh.nirgudkar@gmail.com <shreesh.nirgudkar@gmail.com>
2020-05-13 16:56:48 +0530

    Now zone api will have searching and sorting

--- backend/strapi/medha/api/zone/services/zone.js
+++ backend/strapi/medha/api/zone/services/zone.js
@@ -1,8 +1,60 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+const { buildQuery } = require("strapi-utils");
+const utils = require("../../../config/utils.js");
+const {
+  ROLE_MEDHA_ADMIN,
+  ROLE_ADMIN,
+  ROLE_ZONAL_ADMIN
+} = require("../../../config/constants.js");
+
+module.exports = {
+  getRoleWiseZones: async (role, filters) => {
+    switch (role) {
+      case ROLE_MEDHA_ADMIN:
+      case ROLE_ADMIN:
+        return await strapi
+          .query("zone")
+          .model.query(
+            buildQuery({
+              model: strapi.models.zone,
+              filters
+            })
+          )
+          .fetchAll()
+          .then(res => res.toJSON());
+      case ROLE_ZONAL_ADMIN:
+        return await strapi
+          .query("zone")
+          .model.query(
+            buildQuery({
+              model: strapi.models.zone,
+              filters
+            })
+          )
+          .where({
+            id: zone
+          })
+          .fetchAll()
+          .then(res => res.toJSON());
+      default:
+        return await strapi
+          .query("zone")
+          .model.query(
+            buildQuery({
+              model: strapi.models.zone,
+              filters
+            })
+          )
+          .fetchAll({
+            columns: ["id", "name"]
+          })
+          .then(res => res.toJSON());
+    }
+  }
+};
