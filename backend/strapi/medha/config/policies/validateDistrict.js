83a7f9a6d53bc35ebceede40d52f123a5fe8fad1
Harsh <harshvardhan.akhare@gmail.com>
2020-06-05 18:25:05 +0530

    state,district,zone and rpc policy updated

--- backend/strapi/medha/config/policies/validateDistrict.js
+++ backend/strapi/medha/config/policies/validateDistrict.js
@@ -8,23 +8,45 @@ module.exports = async (ctx, next) => {
   // Add your own logic here.
   console.log("In validateDistrict policy.");
   const { name, state } = ctx.request.body;
+  if (ctx.request.method === "POST") {
+    if (!name) return ctx.response.badRequest("Name field is missing");
+    if (!state) return ctx.response.badRequest("State is required");
 
-  if (!name) return ctx.response.badRequest("Name field is missing");
-  if (!state) return ctx.response.badRequest("State is required");
+    if (state) {
+      const stateId = typeof state === "number" ? state : state.id;
+      const isStateValid = await strapi
+        .query("state", PLUGIN)
+        .findOne({ id: stateId });
 
-  if (state) {
-    const stateId = typeof state === "number" ? state : state.id;
-    const isStateValid = await strapi
-      .query("state", PLUGIN)
-      .findOne({ id: stateId });
+      if (!isStateValid) return ctx.response.badRequest("State is invalid");
+    }
 
-    if (!isStateValid) return ctx.response.badRequest("State is invalid");
+    const district = await strapi
+      .query("district", PLUGIN)
+      .findOne({ name: name, state: state });
+
+    if (district) return ctx.response.badRequest("District already Exist");
+    await next();
   }
+  if (ctx.request.method === "PUT") {
+    const { id } = ctx.params;
+    if (!name) return ctx.response.badRequest("Name field is missing");
+    if (!state) return ctx.response.badRequest("State is required");
 
-  const district = await strapi
-    .query("district", PLUGIN)
-    .findOne({ name: name, state: state });
+    if (state) {
+      const stateId = typeof state === "number" ? state : state.id;
+      const isStateValid = await strapi
+        .query("state", PLUGIN)
+        .findOne({ id: stateId });
 
-  if (district) return ctx.response.badRequest("District already Exist");
-  await next();
+      if (!isStateValid) return ctx.response.badRequest("State is invalid");
+    }
+
+    const district = await strapi
+      .query("district", PLUGIN)
+      .find({ id_nin: [id], name: name, state: state });
+
+    if (district) return ctx.response.badRequest("District already Exist");
+    await next();
+  }
 };
--- backend/strapi/medha/config/policies/validateState.js
+++ backend/strapi/medha/config/policies/validateState.js
@@ -9,10 +9,24 @@ module.exports = async (ctx, next) => {
   console.log("In validateState policy.");
 
   const { name } = ctx.request.body;
-  if (!name) return ctx.response.badRequest("Name field is missing");
+  if (ctx.request.method === "POST") {
+    if (!name) return ctx.response.badRequest("Name field is missing");
 
-  const state = await strapi.query("state", PLUGIN).findOne({ name: name });
-  if (state)
-    return ctx.response.badRequest("State already exist with name " + name);
-  await next();
+    const state = await strapi.query("state", PLUGIN).findOne({ name: name });
+    if (state)
+      return ctx.response.badRequest("State already exist with name " + name);
+    await next();
+  }
+
+  if (ctx.request.method === "PUT") {
+    const { id } = ctx.params;
+    if (!name) return ctx.response.badRequest("Name field is missing");
+
+    const state = await strapi
+      .query("state", PLUGIN)
+      .find({ id_nin: [id], name: name });
+    if (state)
+      return ctx.response.badRequest("State already exist with name " + name);
+    await next();
+  }
 };
