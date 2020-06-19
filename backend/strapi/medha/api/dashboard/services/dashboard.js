36b234328b60364aabad1f94cb9d7cb125aa4a3a
shreesh.nirgudkar@gmail.com <shreesh.nirgudkar@gmail.com>
2020-06-19 18:16:34 +0530

    dashboard services

--- backend/strapi/medha/api/dashboard/services/dashboard.js
+++ backend/strapi/medha/api/dashboard/services/dashboard.js
@@ -1,8 +1,22 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  getPlacementCount: async orgId => {},
+
+  getPlacementAttendedCount: async orgId => {},
+
+  getPlacementPlacedCount: async orgId => {},
+
+  getPlacementSelectedCount: async orgId => {},
+
+  getPlacementStudentFeedbackCount: async orgId => {},
+
+  getPlacementTPOFeedbackCount: async orgId => {},
+
+  getPlacementCollegeFeedbackCount: async orgId => {}
+};
