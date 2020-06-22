7ea88fd83ed19386e0612e979485c0d28f2ee242
shreesh.nirgudkar@gmail.com <shreesh.nirgudkar@gmail.com>
2020-06-22 11:13:45 +0530

    Added placement service for dashboard

--- backend/strapi/medha/api/dashboard/services/dashboard.js
+++ backend/strapi/medha/api/dashboard/services/dashboard.js
@@ -5,8 +5,70 @@
  * to customize this service
  */
 
+const utils = require("../../../config/utils");
+const { PLUGIN, DASHBOARD_START_DATE } = require("../../../config/constants");
+const _ = require("lodash");
+const moment = require("moment");
+
 module.exports = {
-  getPlacementCount: async orgId => {},
+  getPlacementCount: async orgId => {
+    const org = await strapi
+      .query("contact", PLUGIN)
+      .findOne({ id: orgId }, [
+        "organization.rpc",
+        "organization.zone",
+        "state"
+      ]);
+
+    // Getting all placements
+    const placements = await strapi
+      .query("event")
+      .model.query({})
+      .fetchAll()
+      .then(model => {
+        const data = model.toJSON();
+        return data.filter(event => {
+          const { contacts } = event;
+          const contactIds = contacts.map(c => c.id);
+          if (_.includes(contactIds, orgId) || contactIds.length == 0) {
+            return event;
+          }
+        });
+      });
+
+    // Getting months between dates
+    const months = utils.getMonthsBetweenDates(DASHBOARD_START_DATE);
+
+    // Grouping placements monthwise
+    const groupByMonth = _.groupBy(placements, placement => {
+      const { start_date_time } = placement;
+      return moment(start_date_time).format("M yyyy");
+    });
+
+    const response = months.reduce((result, m) => {
+      const [month, year] = m.split(" ");
+      const data = groupByMonth[m];
+      result[m] = {
+        month: parseInt(month),
+        year: parseInt(year),
+        Placement: data ? data.length : 0,
+        rpc:
+          (org.organization &&
+            org.organization.rpc &&
+            org.organization.rpc.name) ||
+          "",
+        zone:
+          (org.organization &&
+            org.organization.zone &&
+            org.organization.zone.name) ||
+          "",
+        state: (org.state && org.state.name) || ""
+      };
+      return result;
+    }, {});
+
+    return response;
+  },
 
   getPlacementAttendedCount: async orgId => {},
 
