9f85d53e225d7a161f306d198e01522e10f849ef
yogesh.dalvi@webaccessglobal.com <Musu30@in>
2020-06-22 10:05:24 +0530

    Committing for merging.

--- backend/strapi/medha/api/dashboard/services/dashboard.js
+++ backend/strapi/medha/api/dashboard/services/dashboard.js
@@ -1,8 +1,21 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/services.html#core-services)
  * to customize this service
  */
+const {
+  PLUGIN,
+  ROLE_MEDHA_ADMIN,
+  ROLE_COLLEGE_ADMIN,
+  ROLE_ZONAL_ADMIN
+} = require("../../../config/constants");
 
-module.exports = {};
+module.exports = {
+  getOverallWorkshops: async orgId => {
+    const data = await strapi
+      .query("activity", PLUGIN)
+      .find({ "contact.organization": orgId });
+    console.log(data);
+  }
+};
