92f037110a558765f18230e866112769564e8ee5
shreesh.nirgudkar@gmail.com <shreesh.nirgudkar@gmail.com>
2020-06-22 13:54:24 +0530

    Added services for placement-attended and placement-selected

--- backend/strapi/medha/api/dashboard/services/dashboard.js
+++ backend/strapi/medha/api/dashboard/services/dashboard.js
@@ -6,7 +6,11 @@
  */
 
 const utils = require("../../../config/utils");
-const { PLUGIN, DASHBOARD_START_DATE } = require("../../../config/constants");
+const {
+  PLUGIN,
+  DASHBOARD_START_DATE,
+  ROLE_STUDENT
+} = require("../../../config/constants");
 const _ = require("lodash");
 const moment = require("moment");
 
@@ -70,15 +74,102 @@ module.exports = {
     return response;
   },
 
-  getPlacementAttendedCount: async orgId => {},
+  getPlacementAttendedCount: async orgId => {
+    return await strapi.services.dashboard.getPlacementCountByStatus(
+      orgId,
+      "is_attendance_verified"
+    );
+  },
 
-  getPlacementPlacedCount: async orgId => {},
-
-  getPlacementSelectedCount: async orgId => {},
+  getPlacementSelectedCount: async orgId => {
+    return await strapi.services.dashboard.getPlacementCountByStatus(
+      orgId,
+      "is_hired_at_event"
+    );
+  },
 
   getPlacementStudentFeedbackCount: async orgId => {},
 
   getPlacementTPOFeedbackCount: async orgId => {},
 
-  getPlacementCollegeFeedbackCount: async orgId => {}
+  getPlacementCollegeFeedbackCount: async orgId => {},
+
+  /**
+   * Getting placement count depending on status for given college
+   */
+  getPlacementCountByStatus: async (orgId, status) => {
+    const org = await strapi
+      .query("contact", PLUGIN)
+      .findOne({ id: orgId }, [
+        "organization.rpc",
+        "organization.zone",
+        "state"
+      ]);
+
+    const placements = await strapi
+      .query("event")
+      .model.query({})
+      .fetchAll()
+      .then(model => {
+        const data = model.toJSON();
+        return data.filter(event => {
+          const { contacts } = event;
+          const contactIds = contacts.map(c => c.id);
+          if (_.includes(contactIds, orgId) || contactIds.length == 0) {
+            return event;
+          }
+        });
+      });
+
+    // Getting placement attendance for given list of events
+    const placementIds = placements.map(placement => placement.id);
+
+    /**
+     * Getting placement status depending on status value
+     * status can be is_attendance_verified or is_hired_at_event
+     */
+    const query = {};
+    query["event_in"] = placementIds;
+    query[status] = true;
+
+    const attendance = await strapi.query("event-registration").find(query);
+
+    // Getting months between dates
+    const months = utils.getMonthsBetweenDates(DASHBOARD_START_DATE);
+
+    // Grouping attendance by month
+    const groupByMonth = _.groupBy(attendance, atd => {
+      const { event } = atd;
+      return moment(event.start_date_time).format("M yyyy");
+    });
+
+    const key =
+      status == "is_attendance_verified"
+        ? "PlacementAttended"
+        : "PlacementSelected";
+
+    const response = months.reduce((result, m) => {
+      const [month, year] = m.split(" ");
+      const data = groupByMonth[m];
+      result[m] = {
+        Month: parseInt(month),
+        Year: parseInt(year),
+        [key]: data ? data.length : 0,
+        rpc:
+          (org.organization &&
+            org.organization.rpc &&
+            org.organization.rpc.id) ||
+          "",
+        zone:
+          (org.organization &&
+            org.organization.zone &&
+            org.organization.zone.id) ||
+          "",
+        state: (org.state && org.state.id) || ""
+      };
+      return result;
+    }, {});
+
+    return response;
+  }
 };
