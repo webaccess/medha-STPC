ed930cdf1403ed04ea35e9ca4795d22833d6d4a4
yogesh-dalvi <Musu30@in>
2020-06-26 13:19:19 +0530

    Committing for merge

--- backend/strapi/medha/api/student-import-csv/services/student-import-csv.js
+++ backend/strapi/medha/api/student-import-csv/services/student-import-csv.js
@@ -28,6 +28,7 @@ module.exports = {
           "DOB",
           "Contact Number",
           "Alternate Contact",
+          "Address",
           "State",
           "District",
           "Email",
